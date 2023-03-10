f4a42c88303d66d5b9acba4843002deb52d6bc65
ndragun92 <nemanjadragun92@gmail.com>
2023-03-10 10:07:31 +0100

    test: final

--- utils/log.spec.ts
+++ utils/log.spec.ts
@@ -2,7 +2,7 @@ import { test, describe, expect, vi } from "vitest";
 import log from "./log";
 
 describe("GlobalButton", () => {
-  test("it shows allert", () => {
+  test("it shows alert", () => {
     window.alert = vi.fn();
     const message = "Hello from alert!";
     const wrapper = log().onAlert(message);
